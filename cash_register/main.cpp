3c07a55e1d7daa36a07e9c5a51228bdda32c8752
Sébastien Lavoie <seblav02@gmail.com>
2016-08-04 16:06:09 -0700

    Initial Commit

---
+++ cash_register/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cash_register
+//
+//  Created by Sebastien Lavoie on 2016-08-04.
+//  Copyright © 2016 Sebastien Lavoie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
