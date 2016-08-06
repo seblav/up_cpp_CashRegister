265b3bced41ababe7505e20680c811b6faac37fc
Sébastien Lavoie <seblav02@gmail.com>
2016-08-06 13:55:12 -0700

    Cash register project for chapter 3

--- cash_register/main.cpp
+++ cash_register/main.cpp
@@ -7,9 +7,60 @@
 //
 
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    int numItems = 0;
+    double totalCost = 0.;
+    double cash;
+    double totalChange;
+    double itemValue;
+    int numDollars;
+    int numQuarters;
+    int numDimes;
+    int numNickel;
+    int numPennies;
+    char moreItems = 'y';
+    int changeLeft;
+    
+    cout<<"Cash register - new transaction"<<endl;
+    
+    while (moreItems == 'y') {
+        cout<<"Enter item price: "<<endl;
+        cin>>itemValue;
+        
+        totalCost += itemValue;
+        numItems += 1;
+        
+        cout<<"Are there more items?"<<endl;
+        cin>>moreItems;
+        
+        cout<<"Your current total is: "<<totalCost<<endl;
+    }
+    
+    cout<<"Enter cash from customer: "<<endl;
+    cin>>cash;
+    
+    totalChange = cash - totalCost;
+    
+    cout<<"Total change back is "<<fixed<<setprecision(2)<<totalChange<<endl;
+    
+    numDollars = int(totalChange);
+    changeLeft = 100 * (totalChange - numDollars);
+    numQuarters = changeLeft / 25;
+    changeLeft -= 25 * numQuarters;
+    numDimes = changeLeft / 10;
+    changeLeft -= 10 * numDimes;
+    numNickel = changeLeft / 5;
+    changeLeft -= 5 * numNickel;
+    numPennies = changeLeft;
+    
+    cout<<"Dollars: "<<numDollars<<endl;
+    cout<<"Quarters: "<<numQuarters<<endl;
+    cout<<"Dimes: "<<numDimes<<endl;
+    cout<<"Nickel: "<<numNickel<<endl;
+    cout<<"Pennies: "<<numPennies<<endl;
+    
     return 0;
 }
